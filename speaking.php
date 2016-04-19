832ee6782c1b5e8f3b27bef483311544018ecc98
Vinay Raghu <rvinay88@gmail.com>
2016-04-19 15:21:10 -0400

    Card layout

--- speaking.php
+++ speaking.php
@@ -10,14 +10,65 @@
 	<link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/joey/style.css">
 </head>
 <body>
+	<section class="speaking-hero">
+		<section class="speaking-hero-copy">
+				<div class="row">
+					<div class="col-sm-6">
+						<h1 class="speaking-hero-title">Speaking & community building</h1>
+					</div>
+					<div class="col-sm-6">
+						<p>I believe in learning through sharing. Most of my web skills were obtained through conference videos, online tutorials and in-person meetups.
+						</p>
+						<p>
 
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<h1>Hello</h1>
+						</p>
+						I am always passionate about establishing and building local tech communities to help and mentor each other. I actively seek opportunities to speak at local meetup groups, help revive ones that aren't active or start groups where none exist.
+					</div>
+				</div>
+		</section>
+	</section>
+	<section class="talks">
+		<section class="pinned-talk">
+			<div class="container">
+				<div class="row mt-l">
+					<div class="card">
+						<div class="col-sm-8 card-image-holder">
+							<img src="<?php echo get_site_url(); ?>/wp-content/themes/joey/images/wpri.jpg" alt="" class="img-responsive card-image-left">
+						</div>
+						<div class="col-sm-4 card-content-holder">
+							<h1 class="recent-talk-title">Utilizing advanced custom fields</h1>
+							<p class="meta">
+								<a href="http://www.meetup.com/WordPressRI/events/228178111/">Wordpress Rhode Island</a>
+							</p>
+							<p class="meta">
+								Apr 12, 2016 (2 weeks ago)
+							</p>
+							<p class="meta">Hands-on workshop</p>
+							<p>I taught attendees of this workshop how to convert a static HTML design to a fully custom wordpress template. </p>
+							<a href="#" class="btn btn-primary">Learn more</a>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
+		</section>
+			<section class="availability">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4">
+							<h1>I am currently not available for talks.</h1>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-4">
+							<p>The whole of 2016, I am taking a break from the speaking routine. I am currently accepting requests for Sep '16 and upwards.</p>
+							<a href="#" class="btn btn-danger">Get in touch</a>
+						</div>
+					</div>
+				</div>
+		</section>
+		<section class="previous-talks"></section>
+	</section>
+
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
